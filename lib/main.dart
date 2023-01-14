b0d09097c66258f2a729c132e85186348a357751
Sukanta Behera <sukantabehera096@gmail.com>
2023-01-14 22:24:58 +0530

    Remove the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class FileZ extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         backgroundColor: Colors.black,
         colorScheme: const ColorScheme.dark(),
